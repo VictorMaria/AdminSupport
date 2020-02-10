9b74e94918f70aefb43aaefc556fedd5d6100b05
Victor Abayomi <victor.abayomi@outlook.com>
2020-02-10 12:44:42 +0100

    set up server

---
+++ config/db.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const db = process.env.mongoURI;
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+export default connectDb;
