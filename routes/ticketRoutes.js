009d574f9fdfaf2537d5e45ac1b86de97dd83861
Victor Abayomi <victor.abayomi@outlook.com>
2020-02-10 15:49:26 +0100

    add input validation

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import TicketController from '../controllers/ticketController';
+import validate from '../validation/validation';
 
 const router = Router();
 const { createTicket, addComment, changeTicketStatus, readTicket, readAllTickets } = TicketController;
 
-router.post('/', createTicket);
+router.post('/', validate.newTicket, createTicket);
 router.patch('/:id/status', changeTicketStatus);
-router.get('/:id', readTicket);
-router.patch('/:id', addComment);
+router.get('/:id', validate.validateId, readTicket);
+router.patch('/:id', validate.validateId, validate.newComment, addComment);
 router.get('/', readAllTickets);
 
 export default router;
